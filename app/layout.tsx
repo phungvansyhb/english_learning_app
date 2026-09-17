357167d3b5ba1095744f03a7b2760dda009b8596
michel_phung <phungvansyhb@gmail.com>
2026-09-17 22:25:46 +0700

    feat: integrate api save activity

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html
-			lang='en'
+			lang='vi'
 			className={`${jakarta.variable}`}>
 			<body className='font-sans antialiased'>
 				{children}
--- components/auth-verify.tsx
+++ components/auth-verify.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { saveActivity } from '@/services/auth';
 import { useAuthStore } from '@/utils/zustand/auth-store';
 import React, { useEffect } from 'react';
 
@@ -6,12 +7,29 @@ type Props = {};
 
 export default function AuthVerify({}: Props) {
 	const { user, getCurrentUser } = useAuthStore();
+
 	useEffect(() => {
 		if (!user) {
-			getCurrentUser();
+			void getCurrentUser();
+			return;
 		}
-		console.log(user);
-	}, [user]);
+
+		const isActiveToday = document.cookie
+			.split('; ')
+			.some((cookie) => cookie.startsWith('today_checkin='));
+
+		if (!isActiveToday) {
+			const now = new Date();
+			const nextMidnight = new Date(now);
+			nextMidnight.setHours(24, 0, 0, 0);
+			const secondsUntilMidnight = Math.ceil(
+				(nextMidnight.getTime() - now.getTime()) / 1000,
+			);
+
+			document.cookie = `today_checkin=true; max-age=${secondsUntilMidnight}; path=/; SameSite=Lax`;
+			void saveActivity();
+		}
+	}, [user, getCurrentUser]);
 
 	return <></>;
 }
